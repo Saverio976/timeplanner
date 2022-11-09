17a978d8f44f945393a5f242a00f5bb0c24f5a1a
Xavier Mitault <xavier.mitault@smobe.fr>
2022-11-09 15:39:15 +0100

    Add helper function

--- src/.timeplanner.sh
+++ src/.timeplanner.sh
@@ -4,12 +4,12 @@ if [[ "$TIMEPLANNER_PATH" == "" ]]; then
     export TIMEPLANNER_PATH="${XDG_CONFIG_HOME:-$HOME/.config}/timeplanner.tp"
 fi
 
-function preexec_timeplanner() {
+function _preexec_timeplanner() {
     cmd_start="$SECONDS"
     cmd_cmd="$3"
 }
 
-function precmd_timeplanner() {
+function _precmd_timeplanner() {
     git_branch=$(git branch --show-current 2>/dev/null)
     if [[ "$git_branch" == "" ]]; then
         return
@@ -25,4 +25,28 @@ function timeplanner_clear() {
     echo "Are you sure ? (Ctrl+C if no)"
     read -r
     true > "$TIMEPLANNER_PATH"
+}
+
+function timeplanner_cat() {
+    cat "$TIMEPLANNER_PATH"
+}
+
+function timeplanner_path() {
+    echo "$TIMEPLANNER_PATH"
+}
+
+function timeplanner_output_time() {
+    echo "Not Implemented... to be continued"
+}
+
+function timeplanner_help() {
+    echo "AVAILIBLE FUNCTIONS: timeplanner_clear, timeplanner_cat, timeplanner_path, timeplanner_output_time"
+    echo
+    echo "timeplanner_clear : clear the log file of time (useful after 'timeplanner_output_time')"
+    echo
+    echo "timeplanner_cat : cat the log file of time"
+    echo
+    echo "timeplanner_path : show path of the log file of time"
+    echo
+    echo "timeplanner_output_time : show a prety table with log time"
 }
